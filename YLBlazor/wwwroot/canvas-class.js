6216d7461a0e5c86fe947338e73c48769615bb82
yos <yos.1965@gmail.com>
2021-03-04 15:50:50 +0200

    Add Test
    Ass CanvasClass defined

--- YLBlazor/wwwroot/canvas-class.js
+++ YLBlazor/wwwroot/canvas-class.js
@@ -1,1 +1,12 @@
-﻿
+﻿let canvasClasses = new Map();
+
+class CanvasClass {
+    
+    constructor(canvasId, img) {
+        this.canvasId = canvasId;
+        this.canvas = document.getElementById(canvasId);
+        this.ctx = this.canvas.getContext("2d");
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
+    }
+}
